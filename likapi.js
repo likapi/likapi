e7c796c917b2ec46e894c25d4d2370569330fc3f
thisiskeanyvy <kinvestisseur@gmail.com>
2021-04-14 22:37:12 +0200

    ajout d'une fonction asynchrone pour l'appel d'APIs

--- likapi.js
+++ likapi.js
@@ -5,20 +5,31 @@
   */
 
 //Appeller la fonction call pour appeller une api (REST)
-//call('https://likapi.gihub.io/api')
 
-async function call(api) {
-	if(typeof api === "undefined") {
-		console.log("Erreur aucune url n'a été définie...")
-	} else {
-		var response = await window.fetch(api)
-			if(response.ok) {
-				var data = await response.json()
-				console.log(data)
-			} else {
-				console.error("Erreur 404 avec la librairie Likapi...")
-				console.log("Impossible d'appeler l'API : ("+response.url+")...")
-  			}
-	}
+const callapi = async (api) => {
+    if(typeof api === "undefined") {
+        console.log("Erreur aucune url n'a été définie...")
+    } else {
+        var response = await window.fetch(api)
+            if(response.ok) {
+                var data = await response.json()
+                return data;
+            } else {
+                console.error("Erreur 404 avec la librairie Likapi...")
+                console.log("Impossible d'appeler l'API : ("+response.url+")...")
+            }
+    }
 }
 
+
+//Appeller la fonction call et entrer l'url de l'api
+//La réponse de l'api est ensuite rangé dans la constante data
+
+function call(api) {
+	;(async () => {
+		const data = await callapi(api)
+    	console.log(data)
+	})()
+}
+
+//Appler l'api avec call("votreurl")
